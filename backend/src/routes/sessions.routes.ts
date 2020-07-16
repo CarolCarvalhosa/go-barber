f8af262e1dd465861dd9fd32b86fee99ee253c25
carolcarvalhosa <ccscarvalhosa@gmail.com>
2020-07-16 00:32:56 -0300

    Exception handling

--- backend/src/routes/sessions.routes.ts
+++ backend/src/routes/sessions.routes.ts
@@ -1,21 +1,18 @@
 import { Router } from 'express';
 import AuthenticateUserService from '../services/AuthenticateUserService';
+import 'express-async-errors';
 
 const sessionssRouter = Router();
 
 sessionssRouter.post('/', async (request, response) => {
-    try {
-        const { email, password } = request.body;
-        const AuthenticateUser = new AuthenticateUserService();
-        const { user, token } = await AuthenticateUser.execute({
-            email,
-            password,
-        });
-        delete user.password;
-        return response.json({ user, token });
-    } catch (err) {
-        return response.status(400).json({ error: err.message });
-    }
+    const { email, password } = request.body;
+    const AuthenticateUser = new AuthenticateUserService();
+    const { user, token } = await AuthenticateUser.execute({
+        email,
+        password,
+    });
+    delete user.password;
+    return response.json({ user, token });
 });
 
 export default sessionssRouter;
--- backend/src/services/UpdateUserAvatarService.ts
+++ backend/src/services/UpdateUserAvatarService.ts
@@ -3,6 +3,7 @@ import path from 'path';
 import fs from 'fs';
 import User from '../models/User';
 import uploadConfig from '../config/upload';
+import AppError from '../errors/AppError';
 
 interface Request {
     user_id: string;
@@ -14,7 +15,10 @@ export default class UpdateUserAvatarService {
         const userRepository = getRepository(User);
         const user = await userRepository.findOne(user_id);
         if (!user) {
-            throw new Error('Only authenticated users can change avatar.');
+            throw new AppError(
+                'Only authenticated users can change avatar.',
+                401,
+            );
         }
         if (user.avatar) {
             const userAvatarFilePath = path.join(
