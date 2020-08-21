1bb0a06052b01dfc5947296cdd363cbf7ebce486
carolcarvalhosa <ccscarvalhosa@gmail.com>
2020-08-20 23:50:00 -0300

    Input and Button components

--- appgobarber/src/pages/SignIn/index.tsx
+++ appgobarber/src/pages/SignIn/index.tsx
@@ -2,12 +2,22 @@ import React from 'react';
 import { Image } from 'react-native';
 import { Container, Title } from './styles';
 import logoImg from '../../assets/logo.png';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
 const SignIn: React.FC = () => (
   <Container>
     <Image source={logoImg} />
-
     <Title>Faça seu logon</Title>
+    <Input name="email" icon="mail" placeholder="E-mail" />
+    <Input name="password" icon="lock" placeholder="Senha" />
+    <Button
+      onPress={() => {
+        console.log('Deu');
+      }}
+    >
+      Entrar
+    </Button>
   </Container>
 );
 
